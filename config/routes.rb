6b0b2cdbac70091152c4625a787968e370f4f0dd
Collins177 <creamcolins@gmail.com>
2024-10-14 16:08:24 +0100

    the user authentication is working correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :users do
+    member do
+      patch :set_admin
+      patch :remove_admin
+    end
+  end
+
+  resources :projects
+  get 'home/index'
+  # root 'home#index'
+  root 'projects#index'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
